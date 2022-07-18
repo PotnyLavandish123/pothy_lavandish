676c1b7b350f1f49b3f52cc58e1a0030d2f56486
PotnyLavandish <ildarbulat1@gmail.com>
2022-07-18 10:34:28 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import org.w3c.dom.ls.LSOutput;
+import java.util.Random;
+import java.util.Scanner;
+//public class main {
+    //public static void main(string[] args) {
+        //System.out.printlh("я гей")
+        //Scanner scan = new scanner(System.in);
+        //string name = "ivan"
+        //char name = '?'
+        //byte name = 23;
+        //short name = 432;
+        //int name = 12341;
+        //long name = 12334567;
+        //float name = 3.432;
+        //double name = 3.3451233142421;
+        //boolean name = true or false;
+        //int ildar = 5;
+        //ildar = ildar - 5;
+        //ildar-= 5;
+        //ildar++;
+        //++ildar;
+        //while (ildar > 10)  {
+        //}
---
+++ src/Task1.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+import java.util.Random;
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner1 =  new Scanner(System.in);
+        int day = 365;
+        if(scanner1.nextInt() < 0){
+            System.out.println("Ты придурок?");
+        }
+       if(scanner1.nextInt() >= 366);{
+            System.out.println("введите число меньше 366");
+        }
+        if
+
+
+
+    }
+
+}
